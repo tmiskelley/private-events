91169502b56647af791c19a75eafeb8de4fa42a5
Timothy Miskelley <miskelleytimothy@gmail.com>
2023-07-18 20:02:01 -0400

    Added view for users attended events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,25 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @created_events = @user.events
+    @events = attending_events
   end
 
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Logged out sucessfully."
     redirect_to root_path
+  end
+
+  private
+
+  def attending_events
+    events = []
+    my_events = EventAttendee.where(user_id: @user.id)
+
+    my_events.each do |event|
+      events << Event.find(event.event_id)
+    end
+
+    events
   end
 end
